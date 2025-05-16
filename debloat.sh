ce117c3fc3a381f9686dd7d995228752441555bd
handingslider <hahandingslider@gmail.com>
2025-05-16 21:15:53 +0300

    new debloat.sh

--- debloat.sh
+++ debloat.sh
@@ -1,98 +1,106 @@
-su -c "pm uninstall -k --user 0 com.miui.phrase"
-su -c "pm uninstall -k --user 0 com.google.android.gms.location.history"
-su -c "pm uninstall -k --user 0 com.miui.bugreport"
-su -c "pm uninstall -k --user 0 com.miui.thirdappassistant"
-su -c "pm uninstall -k --user 0 com.unionpay.tsmservice.mi"
-su -c "pm uninstall -k --user 0 com.miui.hybrid"
-su -c "pm uninstall -k --user 0 com.google.android.marvin.talkback"
-su -c "pm uninstall -k --user 0 com.miui.yellowpage"
-su -c "pm uninstall -k --user 0 com.android.adservices.api"
-su -c "pm uninstall -k --user 0 com.xiaomi.aiasst.service"
-su -c "pm uninstall -k --user 0 com.miui.analytics"
-su -c "pm uninstall -k --user 0 com.mediatek.atmwifimeta"
-su -c "pm uninstall -k --user 0 com.android.bluetoothmidiservice"
-su -c "pm uninstall -k --user 0 com.android.browser"
-su -c "pm uninstall -k --user 0 com.android.calllogbackup"
-su -c "pm uninstall -k --user 0 com.xiaomi.cameratools"
-su -c "pm uninstall -k --user 0 com.miui.carlink"
-su -c "pm uninstall -k --user 0 com.miui.cloudbackup"
-su -c "pm uninstall -k --user 0 com.android.health.connect.backuprestore"
-su -c "pm uninstall -k --user 0 com.android.inputsettings.overlay.miui"
-su -c "pm uninstall -k --user 0 com.android.managedprovisioning.overlay"
-su -c "pm uninstall -k --user 0 com.android.microdroid.empty_payload"
-su -c "pm uninstall -k --user 0 com.android.ondevicepersonalization.services"
-su -c "pm uninstall -k --user 0 com.android.overlay.gmscontactprovider"
-su -c "pm uninstall -k --user 0 com.android.overlay.gmssettingprovider"
-su -c "pm uninstall -k --user 0 com.android.overlay.gmssettings"
-su -c "pm uninstall -k --user 0 com.android.overlay.gmstelecomm"
-su -c "pm uninstall -k --user 0 com.android.overlay.gmstelephony"
-su -c "pm uninstall -k --user 0 com.android.providers.partnerbookmarks"
-su -c "pm uninstall -k --user 0 com.android.settings.overlay.miui"
-su -c "pm uninstall -k --user 0 com.android.stk.overlay.miui"
-su -c "pm uninstall -k --user 0 com.android.systemui.overlay.miui"
-su -c "pm uninstall -k --user 0 com.android.virtualmachine.res"
-su -c "pm uninstall -k --user 0 com.google.android.cellbroadcastreceiver.overlay.miui"
-su -c "pm uninstall -k --user 0 com.google.android.cellbroadcastservice.overlay.miui"
-su -c "pm uninstall -k --user 0 com.miui.miwallpaper.overlay.customize"
-su -c "pm uninstall -k --user 0 com.miui.settings.rro.device.type.overlay"
-su -c "pm uninstall -k --user 0 com.miui.system.overlay"
-su -c "pm uninstall -k --user 0 com.miui.voiceassistoverlay"
-su -c "pm uninstall -k --user 0 com.miui.wallpaper.overlay.customize"
-su -c "pm uninstall -k --user 0 com.debug.loggerui"
-su -c "pm uninstall -k --user 0 com.xiaomi.mirror"
-su -c "pm uninstall -k --user 0 com.xiaomi.digitalkey"
-su -c "pm uninstall -k --user 0 com.miui.greenguard"
-su -c "pm uninstall -k --user 0 com.xiaomi.location.fused"
-su -c "pm uninstall -k --user 0 com.goodix.fingerprint.setting"
-su -c "pm uninstall -k --user 0 com.google.android.onetimeinitializer"
-su -c "pm uninstall -k --user 0 com.android.healthconnect.controller"
-su -c "pm uninstall -k --user 0 com.mediatek.lbs.em2.ui"
-su -c "pm uninstall -k --user 0 com.miui.vsimcore"
-su -c "pm uninstall -k --user 0 com.mediatek.mdmconfig"
-su -c "pm uninstall -k --user 0 com.miui.voiceassist"
-su -c "pm uninstall -k --user 0 com.xiaomi.aiasst.vision"
-su -c "pm uninstall -k --user 0 com.xiaomi.payment"
-su -c "pm uninstall -k --user 0 com.xiaomi.mi_connect.service"
-su -c "pm uninstall -k --user 0 com.miui.accessibility"
-su -c "pm uninstall -k --user 0 com.miui.securityinputmethod"
-su -c "pm uninstall -k --user 0 com.xiaomi.ab"
-su -c "pm uninstall -k --user 0 com.miui.micloudsync"
-su -c "pm uninstall -k --user 0 com.xiaomi.metoknlp"
-su -c "pm uninstall -k --user 0 com.xiaomi.otrpbroker"
-su -c "pm uninstall -k --user 0 com.android.quicksearchbox"
-su -c "pm uninstall -k --user 0 com.xiaomi.security.onetrack"
-su -c "pm uninstall -k --user 0 com.miui.tsmclient"
-su -c "pm uninstall -k --user 0 com.miui.nextpay"
-su -c "pm uninstall -k --user 0 com.tencent.soter.soterserver"
-su -c "pm uninstall -k --user 0 com.miui.daemon"
-su -c "pm uninstall -k --user 0 com.android.traceur"
-su -c "pm uninstall -k --user 0 com.miui.uireporter"
-su -c "pm uninstall -k --user 0 com.miui.voicetrigger"
-su -c "pm uninstall -k --user 0 com.xiaomi.aireco"
-su -c "pm uninstall -k --user 0 com.miui.cloudservice"
-su -c "pm uninstall -k --user 0 com.xiaomi.mis"
-su -c "pm uninstall -k --user 0 com.sohu.inputmethod.sogou.xiaomi"
-su -c "pm uninstall -k --user 0 com.miui.systemAdSolution"
-su -c "pm uninstall -k --user 0 com.xiaomi.gamecenter.sdk.service"
-su -c "pm uninstall -k --user 0 com.miui.contentcatcher"
-su -c "pm uninstall -k --user 0 com.xiaomi.barrage"
-su -c "pm uninstall -k --user 0 com.android.federatedcompute.services"
-su -c "pm uninstall -k --user 0 com.android.systemui.accessibility.accessibilitymenu"
-su -c "pm uninstall -k --user 0 com.mediatek.MtkSettingsResOverlay"
-su -c "pm uninstall -k --user 0 com.mediatek.SettingsProviderResOverlay"
-su -c "pm uninstall -k --user 0 com.mediatek.voiceunlock"
-su -c "pm uninstall -k --user 0 com.miui.securitycenter.securitycenter_phone_overlay.config.overlay"
-su -c "pm uninstall -k --user 0 com.miui.settings.rro.device.hide.statusbar.overlay"
-su -c "pm uninstall -k --user 0 com.xiaomi.touchservice"
-su -c "pm uninstall -k --user 0 com.miui.newhome"
-su -c "pm uninstall -k --user 0 org.ifaa.aidl.manager"
-su -c "pm uninstall -k --user 0 com.iflytek.inputmethod.miui"
-su -c "pm uninstall -k --user 0 com.mediatek.magtapp"
-su -c "pm uninstall -k --user 0 com.mediatek.mdmlsample"
-su -c "pm uninstall -k --user 0 com.xiaomi.mibrain.speech"
-su -c "pm uninstall -k --user 0 com.xiaomi.mi_connect_service"
-su -c "pm uninstall -k --user 0 cn.wps.moffice_eng.xiaomi.lite"
-su -c "pm uninstall -k --user 0 com.miui.newmidrive"
-su -c "pm uninstall -k --user 0 com.mfashiongallery.emag"
-su -c "pm uninstall -k --user 0 com.xiaomi.macro"
-su -c "pm uninstall -k --user 0 com.xiaomi.trustservice"
+#!/system/bin/sh
+
+UNINSTALL="
+com.miui.phrase
+com.google.android.gms.location.history
+com.miui.bugreport
+com.miui.thirdappassistant
+com.unionpay.tsmservice.mi
+com.miui.hybrid
+com.google.android.marvin.talkback
+com.miui.yellowpage
+com.android.adservices.api
+com.xiaomi.aiasst.service
+com.miui.analytics
+com.mediatek.atmwifimeta
+com.android.bluetoothmidiservice
+com.android.browser
+com.android.calllogbackup
+com.xiaomi.cameratools
+com.miui.carlink
+com.miui.cloudbackup
+com.android.health.connect.backuprestore
+com.android.inputsettings.overlay.miui
+com.android.managedprovisioning.overlay
+com.android.microdroid.empty_payload
+com.android.ondevicepersonalization.services
+com.android.overlay.gmscontactprovider
+com.android.overlay.gmssettingprovider
+com.android.overlay.gmssettings
+com.android.overlay.gmstelecomm
+com.android.overlay.gmstelephony
+com.android.providers.partnerbookmarks
+com.android.settings.overlay.miui
+com.android.stk.overlay.miui
+com.android.systemui.overlay.miui
+com.android.virtualmachine.res
+com.google.android.cellbroadcastreceiver.overlay.miui
+com.google.android.cellbroadcastservice.overlay.miui
+com.miui.miwallpaper.overlay.customize
+com.miui.settings.rro.device.type.overlay
+com.miui.system.overlay
+com.miui.voiceassistoverlay
+com.miui.wallpaper.overlay.customize
+com.debug.loggerui
+com.xiaomi.mirror
+com.xiaomi.digitalkey
+com.miui.greenguard
+com.xiaomi.location.fused
+com.goodix.fingerprint.setting
+com.google.android.onetimeinitializer
+com.android.healthconnect.controller
+com.mediatek.lbs.em2.ui
+com.miui.vsimcore
+com.mediatek.mdmconfig
+com.miui.voiceassist
+com.xiaomi.aiasst.vision
+com.xiaomi.payment
+com.xiaomi.mi_connect.service
+com.miui.accessibility
+com.miui.securityinputmethod
+com.xiaomi.ab
+com.miui.micloudsync
+com.xiaomi.metoknlp
+com.xiaomi.otrpbroker
+com.android.quicksearchbox
+com.xiaomi.security.onetrack
+com.miui.tsmclient
+com.miui.nextpay
+com.tencent.soter.soterserver
+com.miui.daemon
+com.android.traceur
+com.miui.uireporter
+com.miui.voicetrigger
+com.xiaomi.aireco
+com.miui.cloudservice
+com.xiaomi.mis
+com.sohu.inputmethod.sogou.xiaomi
+com.miui.systemAdSolution
+com.xiaomi.gamecenter.sdk.service
+com.miui.contentcatcher
+com.xiaomi.barrage
+com.android.federatedcompute.services
+com.android.systemui.accessibility.accessibilitymenu
+com.mediatek.MtkSettingsResOverlay
+com.mediatek.SettingsProviderResOverlay
+com.mediatek.voiceunlock
+com.miui.securitycenter.securitycenter_phone_overlay.config.overlay
+com.miui.settings.rro.device.hide.statusbar.overlay
+com.xiaomi.touchservice
+com.miui.newhome
+org.ifaa.aidl.manager
+com.iflytek.inputmethod.miui
+com.mediatek.magtapp
+com.mediatek.mdmlsample
+com.xiaomi.mibrain.speech
+com.xiaomi.mi_connect_service
+cn.wps.moffice_eng.xiaomi.lite
+com.miui.newmidrive
+com.mfashiongallery.emag
+com.xiaomi.macro
+com.xiaomi.trustservice
+"
+
+for pkg in $UNINSTALL; do
+    su -c "pm uninstall -k --user 0 $pkg"
+done
